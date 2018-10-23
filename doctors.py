61882fdc861926ed43e0452f27786475804c4b39
Tomek <tglonkowski@gmail.com>
2018-10-23 19:04:23 +0200

    add function 'showPersonalDataL' for doctors

---
+++ doctors.py
@@ -0,0 +1,21 @@
+def showPersonalDataL(self, login, haslo):
+    self.login = login
+    self.haslo = haslo
+
+    self.c.execute("SELECT * FROM dane_lekarz where login=%s and haslo=%s", (login, haslo))
+
+    print("| %8s | %8s | %19s | %5s | %5s | %8s | %10s |" % ("Imię", "Nazwisko", "Ulica", "Nr budynku",
+                                                            "Nr lokalu", "Kod pocztowy", "Miasto"))
+    dane = self.c.fetchall()
+
+    for row in dane:
+        imie = row[2]
+        nazwisko = row[3]
+        ulica = row[4]
+        nr_budynku = row[5]
+        nr_lokalu = row[6]
+        kod_pocztowy = row[7]
+        miasto = row[8]
+
+        print("| %8s | %8s | %8s | %10s | %9s | %12s | %10s |" % (imie, nazwisko, ulica, nr_budynku, nr_lokalu,
+                                                                  kod_pocztowy, miasto))

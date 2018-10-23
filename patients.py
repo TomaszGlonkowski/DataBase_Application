05009875b4fa282995edaec7ce0bfbb5a4025641
Tomek <tglonkowski@gmail.com>
2018-10-23 18:59:18 +0200

    create reservation process of visit

--- patients.py
+++ patients.py
@@ -58,3 +58,85 @@ def showVisit(self, login, haslo):
 
         print("| %8s | %10s | %12s | %23s | %15s" % (imie, nazwisko, data_wizyty, choroba,
                                                      imie_l + " " + nazwisko_l))
+
+def showDisease(self):
+    self.c.execute("select id_choroba, nazwa_choroby from choroba order by id_choroba")
+
+    print("| %3s | %7s " % ("ID", "Choroba"))
+
+    dane = self.c.fetchall()
+
+    for row in dane:
+        id = row[0]
+        nazwa_choroby = row[1]
+
+        print("| %3s | %5s " % (id, nazwa_choroby))
+
+
+def idPatient(self, login, haslo):
+    self.login = login
+    self.haslo = haslo
+
+    self.c.execute("SELECT * from id_pacjent where login=%s and haslo=%s", (login, haslo))
+
+    print("| %3s | %10s | %10s " % ("ID", "Imię", "Nazwisko"))
+
+    dane = self.c.fetchall()
+
+    for row in dane:
+        id = row[2]
+        imie = row[3]
+        nazwisko = row[4]
+
+        print("| %3s | %10s | %10s " % (id, imie, nazwisko))
+
+
+def specialization(self):
+    self.c.execute("SELECT * from specjalizacja order by id_specjalizacja")
+
+    print("| %3s | %12s" % ("ID", "Specjalizacja"))
+
+    dane = self.c.fetchall()
+
+    for row in dane:
+        id = row[0]
+        specjalizacja = row[1]
+
+        print("| %3s | %13s" % (id, specjalizacja))
+
+
+def doctor_specialization(self, specjalizacja):
+    self.specjalizacja = specjalizacja
+
+    self.c.execute("SELECT * FROM lekarze_specjalizacje where id_specjalizacja=%s", specjalizacja)
+
+    print("| %3s | %6s" % ("ID", "Lekarz"))
+
+    dane = self.c.fetchall()
+
+    for row in dane:
+        id = row[0]
+        imie = row[1]
+        nazwisko = row[2]
+
+        print("| %3s | %8s" % (id, imie + " " + nazwisko))
+
+
+def addVisit(self, login, haslo):
+    self.login = login
+    self.haslo = haslo
+
+    idPatient(self, login, haslo)
+    id = input("Podaj swoje ID: ")
+    specialization(self)
+    specjalizacja = input("Jaka specjalizacja lekarza Cie interesuję? (Podaj numer): ")
+    doctor_specialization(self, specjalizacja)
+    lekarz = input("Do jakiego lekarza umówić wizytę?(Podaj numer): ")
+    showDisease(self)
+    choroba = input("Jaka choroba?(Podaj numer): ")
+    data = input("Podaj datę wizyty: ")
+
+    self.c.execute("INSERT INTO wizyta (id_pacjent ,id_lekarz, id_choroba, data_wizyty) VALUES (%s ,%s, %s, %s)"
+                   , (id, lekarz, choroba, data))
+
+    self.transaction()
